2d2dd8f323d68225622377c24b5d05eafaab6e13
Luiz Fernando da Silva <luiz.fernando.tech@gmail.com>
2021-02-06 20:05:08 +0000

    hr-worker as a Config server Client

--- hr-worker/src/main/java/com/luizfernandotech/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/luizfernandotech/hrworker/resources/WorkerResource.java
@@ -5,11 +5,10 @@ import com.luizfernandotech.hrworker.repositories.WorkerRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.env.Environment;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -19,11 +18,20 @@ public class WorkerResource {
 
     private static Logger logger = LoggerFactory.getLogger(WorkerResource.class);
 
+    @Value("${test.config}")
+    private String testConfig;
+
     @Autowired
     private Environment environment;
 
     @Autowired
     private WorkerRepository repository;
+
+    @GetMapping(value = "/configs")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void getConfigs() {
+        logger.info("CONFIG = " + testConfig);
+    }
 
     @GetMapping
     public List<Worker> findAll() {
